5848a2525986f390f202421e14a2d261fc27bf2c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-26 18:55:08 +0530

    Added Firebase Config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAPaBFIyCpAlcNwFUpauCuOrvD82urYTI0",
+    authDomain: "superchat-99d1b.firebaseapp.com",
+    databaseURL: "https://superchat-99d1b.firebaseio.com",
+    projectId: "superchat-99d1b",
+    storageBucket: "superchat-99d1b.appspot.com",
+    messagingSenderId: "597647427370",
+    appId: "1:597647427370:web:5c4796020d537395188053"
+};
