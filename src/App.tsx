64dc710a3b8d753b7f84fddd7023dc3ba185da42
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-26 19:03:51 +0530

    Added SignIn And ChatRoom Components

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,28 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'
 import 'firebase/auth'
 import {firebaseConfig} from './firebaseConfig'
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+
+import ChatRoom from "./components/ChatRoom";
+import SignIn from "./components/SignIn";
 
 firebase.initializeApp(firebaseConfig)
 
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+
+    const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header>
       </header>
+
+        <section>
+            {user ? <ChatRoom /> : <SignIn />}
+        </section>
     </div>
   );
 }
---
+++ src/components/ChatRoom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function ChatRoom(){
+
+    return(
+        <h1>Chat Room</h1>
+    )
+
+}
+
+export default ChatRoom;
